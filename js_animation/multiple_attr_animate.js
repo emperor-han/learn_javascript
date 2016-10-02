e9ce70d1feaf55b8d74213c8528408ec6ebfa404
han.some <han.some.htx@gmail.com>
2016-10-02 17:47:05 +0800

    modify the file multi_attr_change.html and multiple_attr_animate.js

--- js_animation/multiple_attr_animate.js
+++ js_animation/multiple_attr_animate.js
@@ -1,16 +1,27 @@
 function startMove(obj,attr,myTarget){//obj 表示给那个对象添加该运动，attr 表示该对象的什么属性运动改变，myTarget表示该对象运动要达到的目标值
 		    
 			clearInterval(obj.timer);
+			var cur_value = 0;
 			
 			obj.timer = setInterval(function(){
-				var speed = (myTarget-parseInt(getStyle(obj,attr)))/10;
-				    speed = speed>0?Math.ceil(speed):Math.floor(speed);//是为了将speed变为整型，
-				if(parseInt(getStyle(obj,attr)) === myTarget){
+				if(attr === 'opacity'){
+                   cur_value = parseFloat(getStyle(obj,attr))*100;
+				}else{
+				   cur_value = parseInt(getStyle(obj,attr));
+				}
+			 var   speed = (myTarget-cur_value)/10;
+                   speed = speed>0?Math.ceil(speed):Math.floor(speed);//是为了将speed变为整型，
+				    
+				if(cur_value === myTarget){
 					
 					clearInterval(obj.timer);
 				}else{
+					if(attr === 'opacity'){
+	                   obj.style[attr] = (cur_value + speed)/100;
+					}else{
+					   obj.style[attr] = cur_value + speed + 'px';
+					}
 					
-					obj.style[attr] = parseInt(getStyle(obj,attr)) + speed + 'px';
 					
 				}
 			},30);
